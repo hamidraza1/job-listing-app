22e946a3fb73b2684c2093d0d6c9513c7b0063d8
hamidraza1 <hrazaa306@gmail.com>
2020-05-31 12:53:38 +0200

    Updates

---
+++ precache-manifest.e745eaef78098b367ff31764cd3d361c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f530d5488f413170bd16ed6f639cd915",
+    "url": "/job-listing-app/index.html"
+  },
+  {
+    "revision": "167b765cd73c6bdb07f8",
+    "url": "/job-listing-app/static/css/main.cef9aafe.chunk.css"
+  },
+  {
+    "revision": "55fdc554c5fb6b3003db",
+    "url": "/job-listing-app/static/js/2.eb22f1da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listing-app/static/js/2.eb22f1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167b765cd73c6bdb07f8",
+    "url": "/job-listing-app/static/js/main.b5f75e61.chunk.js"
+  },
+  {
+    "revision": "ace1dafd6def7b3bb8b1",
+    "url": "/job-listing-app/static/js/runtime-main.058464f5.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listing-app/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
